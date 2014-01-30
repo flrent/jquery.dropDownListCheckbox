096540e2a2138c66e3373636b23963472a2dcc13
claudius108 <claudius.teodorescu@gmail.com>
2014-01-30 11:59:09 +0100

    Added support for general selector for 'checkboxCls', and for adding checked sign for selected option.

--- jquery.dropdownlistcheckbox.js
+++ jquery.dropdownlistcheckbox.js
@@ -1,48 +1,55 @@
-(function ($) {
+(function($) {
 
-    $.fn.dropDownListCheckbox = function (opts) {
-        var item = this;
-        var opts = opts || {
-        	containerCls:'cccccc',
-			checkboxCls:'ddlcb-checkboxCase',
-			arrowCls:'ddlcb-right'
+	$.fn.dropDownListCheckbox = function(opts) {
+		var item = this;
+		var opts = opts || {
+			containerCls : 'cccccc',
+			checkboxCls : '.ddlcb-checkboxCase',
+			arrowCls : 'ddlcb-right'
 		};
 
-        $(opts.containerCls).click(function (event) {
-        	event.stopPropagation();
-            var $tgt = $(event.target);
-            if ($tgt.hasClass(opts.containerCls) || $tgt.hasClass(opts.arrowCls)) {
-                var menu = $(this).children("ul");
-                if (menu.is(':visible')) {
-                    menu.hide();
-            		$("html").unbind("click");
-                }
-                else {
-                    menu.show();
+		$(opts.containerCls).click(
+				function(event) {
+					event.stopPropagation();
+					var $tgt = $(event.target);
+					if ($tgt.hasClass(opts.containerCls)
+							|| $tgt.hasClass(opts.arrowCls)) {
+						var menu = $(this).children("ul");
+						if (menu.is(':visible')) {
+							menu.hide();
+							$("html").unbind("click");
+						} else {
+							menu.show();
 
-		            $('html').bind("click", function(event) {
-		            	$("."+opts.containerCls).children("ul").hide();
-		            	console.log("Clic on html"+event.timeStamp);
-		            	$("html").unbind("click");
-					});
-                }
-            }
-        });
+							$('html').bind("click", function(event) {
+								$(opts.containerCls).children("ul").hide();
+								console.log("Clic on html" + event.timeStamp);
+								$("html").unbind("click");
+							});
+						}
+					}
+				});
 
-        $("." + opts.checkboxCls).click(function () {
-            if ($(this).hasClass("ddlcb-checked")) {
-                $(this).removeClass("ddlcb-checked");
-            }
-            else {
-                $(this).addClass("ddlcb-checked");
-            }
-        });
+		$(opts.checkboxCls).click(function() {
+			if ($(this).hasClass("ddlcb-checked")) {
+				$(this).removeClass("ddlcb-checked");
+			} else {
+				$(this).addClass("ddlcb-checked");
+			}
+		});
 
-        this.children("ul").children("li").click(function () {
-            $(this).parent("ul").hide();
-            $("html").unbind("click");
-        });
+		this.children("ul").children("li").click(function() {
+			var $this = $(this);
+			$this.parent("ul").hide();
+			$("html").unbind("click");
+			if ($this.hasClass("ddlcb-checked")) {
+				$this.removeClass("ddlcb-checked");
+			} else {
+				$this.parent("ul").children("li").removeClass("ddlcb-checked");
+				$this.addClass("ddlcb-checked");
+			}
+		});
 
-        return this;
-    };
+		return this;
+	};
 })(jQuery);
